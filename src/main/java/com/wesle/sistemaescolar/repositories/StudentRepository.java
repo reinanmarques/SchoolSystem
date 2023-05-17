4a6076c2e87f9e608469598603fba47a96b99568
reinanmarques <wesleremarques@gmail.com>
2023-05-17 00:59:50 -0300

    created findAll Student

---
+++ src/main/java/com/wesle/sistemaescolar/repositories/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.wesle.sistemaescolar.repositories;
+
+import com.wesle.sistemaescolar.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+}
--- src/main/java/com/wesle/sistemaescolar/services/serviceAction/StudentServiceImp.java
+++ src/main/java/com/wesle/sistemaescolar/services/serviceAction/StudentServiceImp.java
@@ -2,17 +2,32 @@ package com.wesle.sistemaescolar.services.serviceAction;
 
 import com.wesle.sistemaescolar.dto.request.RegisterStudentDTO;
 import com.wesle.sistemaescolar.dto.response.StudentDTO;
+import com.wesle.sistemaescolar.entities.Student;
+import com.wesle.sistemaescolar.repositories.StudentRepository;
 import com.wesle.sistemaescolar.services.contract.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class StudentServiceImp implements StudentService {
+
+    final
+    StudentRepository repository;
+
+    public StudentServiceImp(StudentRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Page<StudentDTO> findAll(Pageable pageable) {
-        return null;
+        Page<Student> pageStudent =  repository.findAll(pageable);
+
+        return pageStudent.map(StudentDTO::new);
     }
     @Override
     public StudentDTO findById(UUID id) {
